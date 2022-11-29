437689793bcd6de37a7fbd30d23526c12363cfce
apostolossvls <apostolossvls@gmail.com>
2022-11-29 23:50:31 +0200

    Project setup
    Added Unity Project. The Third Person (Core) template was picked. It automatically adds a third person character within a prototype space. It uses the Universal Render Pipeline (URP).
    
    *Imported POLYGON SciFi Space package.
    *Imported Simple FX - Cartoon Particles package.
    
    Added test scripts (Shrine.cs & ElementalController.cs). They will be deleted and remade during class time.
    
    Added Elemental Character prefabs (Normal & Fire).

---
+++ VideoGameProject/Assets/Scripts/Shrine.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrine : MonoBehaviour
+{
+    public GameObject body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ElementController elementC = other.GetComponent<ElementController>();
+        if (elementC != null)
+        {
+            StartCoroutine(ChangeBodies(elementC));
+            /*
+            GameObject newBody = Instantiate(body, elementC.currentBody.transform.parent);
+            newBody.transform.localPosition = Vector3.zero;
+            newBody.transform.localRotation = Quaternion.identity;
+
+            //elementC.transform.SetAsLastSibling();
+            Destroy(elementC.currentBody);
+
+            elementC.currentBody = newBody;
+
+            //elementC.GetComponent<Animator>().enabled = false;
+            //yield return new WaitForEndOfFrame();
+            elementC.GetComponent<Animator>().Rebind();
+            //elementC.GetComponent<Animator>().enabled = true;
+            */
+        }
+    }
+
+    IEnumerator ChangeBodies(ElementController elementC)
+    {
+        GameObject newBody = Instantiate(body, elementC.currentBody.transform.parent);
+        newBody.transform.localPosition = Vector3.zero;
+        newBody.transform.localRotation = Quaternion.identity;
+        SkinnedMeshRenderer newMeshRenderer = newBody.GetComponentInChildren<SkinnedMeshRenderer>();
+        newMeshRenderer.enabled = false;
+
+        Destroy(elementC.currentBody);
+        elementC.transform.SetAsLastSibling();
+
+        elementC.currentBody = newBody;
+
+        //elementC.GetComponent<Animator>().enabled = false;
+        yield return null;
+        elementC.GetComponent<Animator>().Rebind();
+        newMeshRenderer.enabled = true;
+        //elementC.GetComponent<Animator>().enabled = true;
+    }
+}
