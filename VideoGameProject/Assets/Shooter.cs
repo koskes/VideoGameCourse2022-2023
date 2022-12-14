bcbc68100c71b4918e878d57257065d804a6a2d0
apostolossvls <apostolossvls@gmail.com>
2022-12-14 08:00:59 +0200

    Lesson 5 pre update 2
    Added Shooter script.
    Changed input handling to both old and new system.

---
+++ VideoGameProject/Assets/Shooter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public Transform castPosition;
+    public float power = 20f;
+    public float cooldownTime = 1.5f;
+    float cooldownTimer;
+    int timeShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeShoot = 0;
+        cooldownTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldownTimer > 0) cooldownTimer -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(1) && cooldownTimer <= 0)
+        {
+            //if (Input.GetMouseButtonDown(0))
+            //if (Input.GetKeyDown(KeyCode.E))
+            //if (Input.GetButtonDown("Attack"))
+
+            Shoot();
+            cooldownTimer = cooldownTime;
+        }
+    }
+
+    void Shoot()
+    {
+        //if (Input.GetMouseButtonDown(0)){
+        timeShoot++;
+        GameObject clone = GameObject.Instantiate(projectilePrefab, transform);
+        clone.transform.position = castPosition.position;
+        clone.SetActive(true);
+        //clone.transform.localPosition = new Vector3(0,0,0);
+        clone.transform.parent = null;
+        //clone.transform.position = transform.position; //100,0,0
+        Destroy(clone, 5f);
+        clone.GetComponent<Rigidbody>().AddForce(transform.forward * power, ForceMode.VelocityChange);
+    }
+}
